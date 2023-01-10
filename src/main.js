bc6dc83ce93e9f008feddacfe6e8236b9261e9dd
pengqixun <864172586@qq.com>
2023-01-10 16:55:06 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/styles/index.css'
+// 加载全局样式
+import '@/styles/index.less'
+
+// 加载Vant 核心组件库
+import Vant from 'vant'
+
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
